636189e68bc24bc6c5d4092ac3865b3d28053893
Nandha <loopmintdigital@gmail.com>
2026-03-21 07:13:25 +0530

    add CSP headers and fix iframe srcDoc

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors *; default-src * 'unsafe-inline' 'unsafe-eval' data: blob:;",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
